c35828ac89a79ed88410e3a1fe5d3b3d5c595aba
wl275 <wl275@duke.edu>
2024-04-18 20:37:38 -0400

    admin page with no users yet

--- ui/src/views/Admin.vue
+++ ui/src/views/Admin.vue
@@ -1,3 +1,53 @@
 <template>
-  admin
+  <div class="task-tracker-container">
+    <!-- 左侧导航栏 -->
+    <aside class="sidebar">
+      <h1>Task Tracker</h1>
+    </aside>
+
+    <!-- Main content area -->
+    <div class="main-content">
+      <button class="sign-out-button" @click="logout">
+        Sign out
+        <span class="icon-logout"></span>
+      </button>
+      <!-- Greeting Section -->
+      <div class="greeting-section">
+        <h2>Hello, Admin!</h2>
+        <p></p>
+        <span class="date">Monday, 18 December 2023</span>
+      </div>
+
+      <section class="task-section">
+        <header class="task-header">
+          <h2>Users</h2>
+        </header>
+
+        <!-- <div class="task-list">
+          <div></div>
+        </div> -->
+      </section>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { onMounted, ref, provide } from "vue";
+import { User } from "../data";
+import { io } from "socket.io-client";
+
+const socket = io(); // 使用你的Socket.IO服务器地址
+
+function logout() {
+  (window.document.getElementById("logoutForm") as HTMLFormElement).submit();
+}
+
+const user = ref({} as User);
+provide("user", user);
+
+onMounted(async () => {
+  user.value = await (await fetch("/api/user")).json();
+});
+</script>
+
+<style scoped lang="scss" , src="../assets/styles/Dashboard.scss"></style>
