c35ccd22a5ff9e53c8fc41450436c1dab86fa743
rui.jiang296@duke.edu <rj153@duke.edu>
2024-04-20 17:26:12 -0400

    dockerfile

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,13 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 test('bypass OIDC login with environment variable', async ({ page }) => {
-  const loginUrl = `http://localhost:8193/api/login?key=foo-bar-baz&user=<testuser>?&role=<user>`;
+  const loginUrl = `http://localhost:8080/api/login?key=foo-bar-baz&user=testuser?&role=user`;
 
   await page.goto(loginUrl);
 
-  await expect(page).toHaveURL('http://localhost:8193');
+  await expect(page).toHaveURL('http://localhost:8080');
 
-  await page.goto('http://localhost:8193/dashboard');
-  await expect(page).toHaveURL('http://localhost:8193/dashboard');
+  await page.goto('http://localhost:8080/dashboard');
+  await expect(page).toHaveURL('http://localhost:8080/dashboard');
 
 })
