c33de7fe59111502bf3c1f2a4cd000f973dcf627
rui.jiang296@duke.edu <rj153@duke.edu>
2024-04-21 18:59:34 -0400

    Update subproject commit in builds/SE6nGW2Vn/0/rj153/cs590-final-project-group-5 Refactor login URL in example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,10 +7,5 @@ test('bypass OIDC login with environment variable', async ({ page }) => {
   const loginUrl = `http://127.0.0.1:31000/api/login?key=foo-bar-baz&user=testuser?&role=user`;
 
   await page.goto(loginUrl);
-
-  await expect(page).toHaveURL('http://127.0.0.1:31000');
-
-  await page.goto('http://127.0.0.1:31000/dashboard');
-  await expect(page).toHaveURL('http://127.0.0.1:31000/dashboard');
-
+  await page.click('text=Add Task');
 })
