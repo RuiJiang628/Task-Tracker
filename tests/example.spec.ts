777152fe064d025c995ba620a682f767500e6c5f
rui.jiang296@duke.edu <rj153@duke.edu>
2024-04-22 14:30:00 -0400

    playwright script click

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,5 +7,5 @@ test('bypass OIDC login with environment variable', async ({ page }) => {
   const loginUrl = `http://127.0.0.1:31000/api/login?key=foo-bar-baz&user=testuser?&role=user`;
 
   await page.goto(loginUrl);
-  await page.click('Add Task');
+  await page.click('text=Add Task');
 })
